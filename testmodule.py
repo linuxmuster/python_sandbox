7bc11482e13300a13876a528b26dd3fff68d33e9
Rüdiger Beck <jeffbeck@web.de>
2022-03-21 00:31:13 +0100

    passing key Value pair to function

--- testmodule.py
+++ testmodule.py
@@ -3,10 +3,15 @@ print('Loading testmodule...')
 
 #### functions ##############################
 def print_data(item,name='default_name'):
-    """Ausgabe von Daten"""
+    """Übergabe mehrerer Daten"""
     print(f"{name} hat {len(name)} Buchstaben")
     print(f"{item} hat {len(item)} Buchstaben")
+    return 0
 
-
+def greeting(data):
+    """Übergabe EINER komplexen Datenstruktur"""
+    print(data) # overview of the data structure
+    print(f"{data['name']} is greeted in the morning with: {data['morning_greeting']}")
+    print(f"{data['name']} is greeted in the evening with: {data['evening_greeting']}")
 
 print('... testmodule loaded')
