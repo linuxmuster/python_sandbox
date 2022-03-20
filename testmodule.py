855f719910141db7c3093edaa0b3417d606eb7ec
Rüdiger Beck <jeffbeck@web.de>
2022-03-20 23:52:41 +0100

    calling function from module file

--- testmodule.py
+++ testmodule.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+print('Loading testmodule...')
+
+#### functions ##############################
 def print_data(item,name='default_name'):
     """Ausgabe von Daten"""
     print(f"{name} hat {len(name)} Buchstaben")
     print(f"{item} hat {len(item)} Buchstaben")
+
+
+
+print('... testmodule loaded')
