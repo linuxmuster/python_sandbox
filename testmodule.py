aebf1c7c3e752bc44ca5ee06bba221c747fb94bb
Rüdiger Beck <jeffbeck@web.de>
2022-03-21 01:32:40 +0100

    keys of keys ...

--- testmodule.py
+++ testmodule.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 print('Loading testmodule...')
 
+import pprint
+
 #### functions ##############################
 def print_data(item,name='default_name'):
     """Übergabe mehrerer Daten"""
@@ -8,10 +10,14 @@ def print_data(item,name='default_name'):
     print(f"{item} hat {len(item)} Buchstaben")
     return 0
 
-def greeting(data):
+def greeting(users,username):
     """Übergabe EINER komplexen Datenstruktur"""
-    print(data) # overview of the data structure
-    print(f"{data['name']} is greeted in the morning with: {data['morning_greeting']}")
-    print(f"{data['name']} is greeted in the evening with: {data['evening_greeting']}")
+    #print(users) # overview of the data structure
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(users)
+    print(f"{users['sAMAccountName'][username]['firstname']} is greeted in the morning with: {users['sAMAccountName'][username]['morning_greeting']}")
+    print(f"{users['sAMAccountName'][username]['firstname']} is greeted in the evening with: {users['sAMAccountName'][username]['evening_greeting']}")
+    print()
+
 
 print('... testmodule loaded')
